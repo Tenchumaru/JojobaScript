5f67dae6284b858b239a973303515010b7697d0f
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-11-24 14:01:48 -0800

    Implement Context value accessors
    Values are integers for now.

--- Context.cpp
+++ Context.cpp
@@ -1,1 +1,31 @@
+#include <stdexcept>
 #include "Context.h"
+
+void Context::AddValue(std::string const& key, Value value) {
+	auto it = values.find(key);
+	if (it == values.end()) {
+		values.insert({ key, value });
+	}
+	throw std::logic_error("cannot add known value");
+}
+
+int Context::GetValue(std::string const& key) {
+	auto it = values.find(key);
+	if (it != values.end()) {
+		return it->second;
+	} else if (outerContext) {
+		return outerContext->GetValue(key);
+	}
+	throw std::logic_error("cannot find value");
+}
+
+void Context::SetValue(std::string const& key, Value value) {
+	// TODO:  add the "const" concept and refuse to set such values.
+	auto it = values.find(key);
+	if (it != values.end()) {
+		it->second = value;
+	} else if (outerContext) {
+		return outerContext->SetValue(key, value);
+	}
+	throw std::logic_error("cannot find key");
+}
--- Context.h
+++ Context.h
@@ -1,13 +1,21 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <unordered_map>
+
+using Value = int;
 
 class Context {
 public:
 	Context(std::shared_ptr<Context> outerContext) : outerContext(std::move(outerContext)) {}
 	Context(Context&&) = default;
 	~Context() = default;
+	void AddValue(std::string const& key, Value value);
+	int GetValue(std::string const& key);
+	void SetValue(std::string const& key, Value value);
 
 private:
+	std::unordered_map<std::string, Value> values;
 	std::shared_ptr<Context> outerContext;
 };
