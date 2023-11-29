916ba5538f6b3c94ffafa327b258d555cc3b9a2e
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-11-28 17:44:28 -0800

    Create the Iterator class
    I will use it in generators and other contexts requiring an iterator.

---
+++ Iterator.cc
@@ -0,0 +1,99 @@
+#include "pch.h"
+#include "Context.h"
+#include "Generator.h"
+#include "Iterator.h"
+#include "Statement.h"
+
+Iterator::Strategy::~Strategy() {}
+
+Iterator::Iterator(std::shared_ptr<Context> outerContext, std::vector<std::pair<std::string, std::string>> const& ids, std::unique_ptr<Expression> const& sourceExpression) : outerContext(outerContext), ids(ids) {
+	Value sourceValue = sourceExpression->GetValue(outerContext);
+	if (std::holds_alternative<std::string>(sourceValue)) {
+		strategy = std::make_unique<StringStrategy>(std::get<std::string>(sourceValue));
+	} else if (std::holds_alternative<std::shared_ptr<Dictionary>>(sourceValue)) {
+		strategy = std::make_unique<DictionaryStrategy>(std::get<std::shared_ptr<Dictionary>>(sourceValue));
+	} else if (std::holds_alternative<std::shared_ptr<Generator>>(sourceValue)) {
+		strategy = std::make_unique<GeneratorStrategy>(std::get<std::shared_ptr<Generator>>(sourceValue));
+	} else if (std::holds_alternative<std::shared_ptr<List>>(sourceValue)) {
+		strategy = std::make_unique<ListStrategy>(std::get<std::shared_ptr<List>>(sourceValue));
+	} else if (std::holds_alternative<std::shared_ptr<Set>>(sourceValue)) {
+		strategy = std::make_unique<SetStrategy>(std::get<std::shared_ptr<Set>>(sourceValue));
+	} else {
+		throw std::runtime_error("source value is not iterable");
+	}
+}
+
+std::shared_ptr<Context> Iterator::operator++() {
+	Value nextValue = ++*strategy;
+	if (std::holds_alternative<nullptr_t>(nextValue)) {
+		return {};
+	}
+	auto context = std::make_shared<Context>(outerContext);
+	if (ids.size() == 1) {
+		context->AddValue(ids.back().first, nextValue);
+	} else if (std::holds_alternative<std::shared_ptr<List>>(nextValue)) {
+		auto&& list = *std::get<std::shared_ptr<List>>(nextValue);
+		if (ids.size() != list.size()) {
+			throw std::runtime_error("incorrect number of comprehension identifiers");
+		}
+		for (size_t i = 0; i < ids.size(); ++i) {
+			context->AddValue(ids[i].first, list[i]);
+		}
+	} else {
+		throw std::runtime_error("incorrect number of comprehension identifiers");
+	}
+	return context;
+}
+
+Value Iterator::DictionaryStrategy::operator++() {
+	if (keys.empty()) {
+		std::ranges::transform(*sourceValue, std::back_inserter(keys), [](auto const& pair) { return pair.first; });
+		keyIt = keys.begin();
+	} else if (keyIt != keys.end()) {
+		++keyIt;
+	}
+	if (keyIt != keys.end()) {
+		if (keys.size() == sourceValue->size()) {
+			auto pairIt = sourceValue->find(*keyIt);
+			if (pairIt != sourceValue->end()) {
+				return std::make_shared<List>(std::vector<Value>{ pairIt->first, pairIt->second });
+			}
+		}
+		throw std::runtime_error("dictionary changed");
+	}
+	return nullptr;
+}
+
+Value Iterator::GeneratorStrategy::operator++() {
+	return ++*sourceValue;
+}
+
+Value Iterator::ListStrategy::operator++() {
+	if (index < 0) {
+		index = 0;
+	} else if (index < sourceValue->size()) {
+		++index;
+	}
+	if (index < sourceValue->size()) {
+		return (*sourceValue)[index];
+	}
+	return nullptr;
+}
+
+std::shared_ptr<List> Iterator::SetStrategy::ConvertToList(std::shared_ptr<Set> sourceValue) {
+	auto&& set = *sourceValue;
+	std::vector<Value> values(set.begin(), set.end());
+	return std::make_shared<List>(std::move(values));
+}
+
+Value Iterator::StringStrategy::operator++() {
+	if (!it.has_value()) {
+		it = sourceValue.begin();
+	} else if (it.value() != sourceValue.end()) {
+		++it.value();
+	}
+	if (it != sourceValue.end()) {
+		return Value(std::string(1, *it.value()));
+	}
+	return nullptr;
+}
---
+++ Iterator.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "Context.h"
+
+class Expression;
+
+class Iterator {
+public:
+	class Strategy {
+	public:
+		virtual ~Strategy() = 0;
+		virtual Value operator++() = 0;
+	};
+
+	class DictionaryStrategy : public Strategy {
+	public:
+		DictionaryStrategy(std::shared_ptr<Dictionary> sourceValue) : sourceValue(std::move(sourceValue)) {}
+		DictionaryStrategy(DictionaryStrategy&&) = default;
+		~DictionaryStrategy() = default;
+
+	private:
+		std::shared_ptr<Dictionary> sourceValue;
+		std::vector<Value> keys; // Keep a collection of keys since an iterator might become invalid if the dictionary changes.
+		std::vector<Value>::const_iterator keyIt;
+
+		Value operator++() override;
+	};
+
+	class GeneratorStrategy : public Strategy {
+	public:
+		GeneratorStrategy(std::shared_ptr<Generator> sourceValue) : sourceValue(std::move(sourceValue)) {}
+		GeneratorStrategy(GeneratorStrategy&&) = default;
+		~GeneratorStrategy() = default;
+
+	private:
+		std::shared_ptr<Generator> sourceValue;
+
+		Value operator++() override;
+	};
+
+	class ListStrategy : public Strategy {
+	public:
+		ListStrategy(std::shared_ptr<List> sourceValue) : sourceValue(std::move(sourceValue)) {}
+		ListStrategy(ListStrategy&&) = default;
+		~ListStrategy() = default;
+
+	private:
+		std::shared_ptr<List> sourceValue;
+		int index = -1; // Use an index since an iterator might become invalid if the list changes.
+
+		Value operator++() override;
+	};
+
+	class SetStrategy : public ListStrategy {
+	public:
+		SetStrategy(std::shared_ptr<Set> sourceValue) : ListStrategy(ConvertToList(sourceValue)) {}
+		SetStrategy(SetStrategy&&) = default;
+		~SetStrategy() = default;
+
+	private:
+		static std::shared_ptr<List> ConvertToList(std::shared_ptr<Set> sourceValue);
+	};
+
+	class StringStrategy : public Strategy {
+	public:
+		StringStrategy(std::string sourceValue) : sourceValue(std::move(sourceValue)) {}
+		StringStrategy(StringStrategy&&) = default;
+		~StringStrategy() = default;
+
+	private:
+		std::string sourceValue;
+		std::optional<std::string::const_iterator> it;
+
+		Value operator++() override;
+	};
+
+	Iterator(std::shared_ptr<Context> outerContext, std::vector<std::pair<std::string, std::string>> const& ids, std::unique_ptr<Expression> const& sourceExpression);
+	Iterator(Iterator&&) = default;
+	~Iterator() = default;
+	std::shared_ptr<Context> operator++();
+
+private:
+	std::shared_ptr<Context> outerContext;
+	std::vector<std::pair<std::string, std::string>> const& ids;
+	std::unique_ptr<Strategy> strategy;
+};
